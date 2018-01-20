e95f7ebdb4270d320213bdf7253f789d7924e454
Nicodemus Swayne <nswayne@nectarom.com>
2018-01-20 16:45:46 -0600

    Add footer to list page.

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,7 +1,28 @@
 import React from 'react';
 import { compose, lifecycle } from 'recompose';
 
-const List = () => <div>List page</div>;
+import internets from '../../resources/icons/tab_internets@2x.png';
+import lunch from '../../resources/icons/tab_lunch@2x.png';
+
+const List = () => (
+    <div className="">
+        <div>Header</div>
+        <div>
+            Body
+            <div>Item</div>
+        </div>
+        <div className="footer">
+            <button type="button" className="button" onClick={() => {}}>
+                <img src={lunch} alt="" />
+                <div>lunch</div>
+            </button>
+            <button type="button" className="button" onClick={() => {}}>
+                <img src={internets} alt="" />
+                <div>internets</div>
+            </button>
+        </div>
+    </div>
+);
 
 const enhance = compose(
     lifecycle({
