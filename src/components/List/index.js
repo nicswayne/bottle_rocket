f25d88e56857be7257ccfb3f0ef4739b90e0c1fc
Nicodemus Swayne <nswayne@nectarom.com>
2018-01-20 18:40:04 -0600

    Add header template.

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -4,10 +4,16 @@ import { compose, lifecycle } from 'recompose';
 import gradient from '../../resources/backgrounds/cellGradientBackground@2x.png';
 import internets from '../../resources/icons/tab_internets@2x.png';
 import lunch from '../../resources/icons/tab_lunch@2x.png';
+import map from '../../resources/icons/icon_map@2x.png';
 
 const List = () => (
     <div>
-        <div>Header</div>
+        <div className="header">
+            <div className="text">
+                <strong>Lunch Tyme</strong>
+                <img className="map" src={map} alt="" />
+            </div>
+        </div>
         <div className="list-body">
             <div className="item">
                 <img
@@ -17,7 +23,7 @@ const List = () => (
                 />
                 <img className="top-img" src={gradient} alt="" />
                 <div>
-                    <div className="name">
+                    <div className="name text">
                         <strong>Restaurant Name</strong>
                         <div className="category">Category Type</div>
                     </div>
@@ -27,11 +33,11 @@ const List = () => (
         <div className="footer">
             <button type="button" className="button" onClick={() => {}}>
                 <img src={lunch} alt="" />
-                <div>lunch</div>
+                <div className="text">lunch</div>
             </button>
             <button type="button" className="button" onClick={() => {}}>
                 <img src={internets} alt="" />
-                <div>internets</div>
+                <div className="text">internets</div>
             </button>
         </div>
     </div>
