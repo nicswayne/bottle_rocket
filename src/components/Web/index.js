8607ffc1093b19e6395a9f5cdf4e2fda2acf2839
Nicodemus Swayne <nswayne@nectarom.com>
2018-01-21 12:12:34 -0600

    Add contact link to web content.

--- src/components/Web/index.js
+++ src/components/Web/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
-const Web = () => <div>Web</div>;
+const Web = () => (
+    <div className="web-body">
+        <iframe
+            src="https://www.bottlerocketstudios.com/contact"
+            title="contact"
+        />
+    </div>
+);
 
 export default Web;
