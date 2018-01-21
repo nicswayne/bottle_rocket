77491fadd8ad1bb5993febf1e83e7c844291741a
Nicodemus Swayne <nswayne@nectarom.com>
2018-01-20 18:14:13 -0600

    Add list page body template.

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import { compose, lifecycle } from 'recompose';
 
+import gradient from '../../resources/backgrounds/cellGradientBackground@2x.png';
 import internets from '../../resources/icons/tab_internets@2x.png';
 import lunch from '../../resources/icons/tab_lunch@2x.png';
 
 const List = () => (
-    <div className="">
+    <div>
         <div>Header</div>
-        <div>
-            Body
-            <div>Item</div>
+        <div className="list-body">
+            <div className="item">
+                <img
+                    className="background-img"
+                    src="http://sandbox.bottlerocketapps.com/BR_iOS_CodingExam_2015_Server/Images/hopdoddy.png"
+                    alt=""
+                />
+                <img className="top-img" src={gradient} alt="" />
+                <div>
+                    <div className="name">
+                        <strong>Restaurant Name</strong>
+                        <div className="category">Category Type</div>
+                    </div>
+                </div>
+            </div>
         </div>
         <div className="footer">
             <button type="button" className="button" onClick={() => {}}>
