1f3c2e4c90fcf2d9f3b041a0d376ce42c600edc7
Nicodemus Swayne <nswayne@nectarom.com>
2018-01-22 11:23:22 -0600

    Add initial Flow setup.

---
+++ src/types/redux.js
@@ -0,0 +1,9 @@
+declare type Action = {|
+    type: string,
+    error?: string,
+    payload?: any,
+|};
+
+declare type Dispatch = (a: Action | Thunk) => any;
+declare type GetState = () => Object;
+declare type Thunk = (d: Dispatch, g: GetState) => any;
